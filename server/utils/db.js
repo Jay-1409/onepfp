adaeff3aa0b762afe1ecf721ddf75c1bc48004d9
Jay Shah <jay_shah@outlook.in>
2026-07-13 18:58:55 +0530

    utils(db): add database connection object

---
+++ server/utils/db.js
@@ -0,0 +1,16 @@
+const oracledb = require("oracledb");
+oracledb.autoCommit = true;
+export const DB = async () => {
+    try {
+        const connection = await oracledb.getConnection({
+            user: process.env.ORACLE_USERNAME,
+            password: process.env.ORACLE_PASSWORD,
+            connectString: process.env.CONNECTION_STRING
+        });
+        console.log("Database connected successfully");
+        return connection;
+    } catch (err) {
+        console.error("Database connection error:", err);
+        throw err;
+    }
+};
